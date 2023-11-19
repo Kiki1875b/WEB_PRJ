5f76ede65796a6b6bdbc450e754802b971e1b812
kiki1875b <kiki1875b>
2023-11-19 19:05:10 +0900

    회원가입

--- register.js
+++ register.js
@@ -1,3 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const userID = document.getElementById('username_field');
+    const password_a = document.getElementById('password_field');
+    const password_b = document.getElementById('vpassword_field');
+    const phone_number = document.getElementById('phoneNum_field');
+    const addressField = document.getElementById('address_field');
+    const specific_addressField = document.getElementById('specific_address_field');
+    const combinedAddressField = document.getElementById('combined_address_field');
+    const email = document.getElementById('email_field');
+    const sex = document.getElementById('sex_field');
     
+    const registerButton = document.getElementById('register_submits');
+
+    function openAddressPopup() {
+        new daum.Postcode({
+            oncomplete: function (data) {
+                // 주소 입력 필드에 선택된 주소 적용
+                addressField.value = data.address;
+            }
+        }).open();
+    }
+
+    registerButton.addEventListener('click', function (event) {
+        
+        event.preventDefault();
+        const username = userID.value;
+        const password = password_a.value;
+        const phoneNum = phone_number.value;
+        const address = addressField.value + " " + specific_addressField.value;
+        const temail = email.value;
+        const tsex = sex.value;
+        
+        console.log(address);
+
+        fetch('/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: `username=${username}&password=${password}&phoneNum=${phoneNum}&address=${address}&email=${temail}&sex=${tsex}`,
+        })
+        .then(response => response.text())
+        .then(data =>{
+            console.log(data);
+        })
+        .catch(error => console.log(error));
+
+
+    
+    });
+
+    addressField.addEventListener('click', openAddressPopup);
 });
--- server.js
+++ server.js
@@ -29,13 +29,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // 정적 파일 제공 (login.html과 login.js)
 //app.use(express.static('public'));
-app.use(express.static('C:/ww/WEB_PRJ'));
+
 
 // 로그인 요청 처리
 app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-
+  
   // MySQL에서 사용자 정보 확인
   const sql = 'SELECT * FROM USER WHERE UID = ? AND PWD = ?';
   db.query(sql, [username, password], (err, result) => {
@@ -48,6 +48,33 @@ app.post('/login', (req, res) => {
     }
   });
 });
+
+
+app.post('/register',(req,res)=> {
+  const{username, password, phoneNum, address, email, sex} = req.body;
+  const insertQuery = 'INSERT INTO USER (UID, PWD, phone_num, Address, Email, `Register Date`, Sex) VALUES (?, ?, ?, ?, ?, CURDATE(), ?)';
+  const values = [username, password, phoneNum, address, email, sex];
+
+  if (!username) {
+    // 클라이언트에서 'username' 값을 전달하지 않았을 경우
+    return res.status(400).send("Bad Request: 'username' is required. A" + req.body.username);
+  }
+
+  db.query(insertQuery, values, (err, results) => {
+    if(err){
+      throw err;
+    }
+
+    if(results.length >0){
+      res.send("회원가입 성공!");
+    }else{
+      res.send("실패");
+    }
+  });
+
+});
+
+app.use(express.static('C:/ww/WEB_PRJ'));
 
 // 서버 시작
 app.listen(port, () => {
